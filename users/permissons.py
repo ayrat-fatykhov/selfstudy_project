8eca960a4a408bd06a7fd5005adbdcbb413f9e20
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-06-26 21:32:36 +0300

    add dockstring for IsModer

--- users/permissons.py
+++ users/permissons.py
@@ -2,5 +2,8 @@ from rest_framework import permissions
 
 
 class IsModer(permissions.BasePermission):
+    """
+    Вводит разрешения в контроллерах для группы'manager'.
+    """
     def has_permission(self, request, view):
         return request.user.groups.filter(name='manager').exists()
