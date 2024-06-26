6d51191f013ce4cfea79c9ba4dfd7d0fd393d80e
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-06-26 20:51:13 +0300

    add view models Chapter, Material, Question, Answer, CheckAnswer in admin

--- lms/admin.py
+++ lms/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from lms.models import Chapter, Material, Question, Answer, CheckAnswer
+
+admin.site.register(Chapter)
+
+admin.site.register(Material)
+
+admin.site.register(Question)
+
+admin.site.register(Answer)
+
+admin.site.register(CheckAnswer)
