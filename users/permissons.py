ca1e859744c4e96516cb655f7667c69bca29059d
Ayrat Fatykhov <airat_91_91@mail.ru>
2024-06-29 18:38:27 +0300

    add space in dockstring for IsModer

--- users/permissons.py
+++ users/permissons.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class IsModer(permissions.BasePermission):
     """
-    Вводит разрешения в контроллерах для группы'manager'.
+    Вводит разрешения в контроллерах для группы 'manager'.
     """
     def has_permission(self, request, view):
         return request.user.groups.filter(name='manager').exists()
